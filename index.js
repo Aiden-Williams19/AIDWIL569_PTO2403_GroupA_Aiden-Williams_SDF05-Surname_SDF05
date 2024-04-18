7217eb3c4a44b3f92713d442201f5cd3f7610855
Aiden Williams <aidenwilliams336@gmail.com>
2024-04-19 01:37:54 +0200

    updated my CSS

--- index.js
+++ index.js
@@ -3,9 +3,44 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 8
+let cards = [firstCard, secondCard] // array - ordered list of items
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
-console.log(sum)
+let cardsEl = document.getElementById("cards-el")
+
+
+function startGame(){
+    renderGame()
+}
+
+function renderGame() {
+
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+    sumEl.textContent = "Sum: " + sum
+    
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message
+}
+
+function newCard(){
+    console.log("Drawing a new card from the deck!")
+    let card = 3
+    sum += card
+    cards.push(card)
+    renderGame()
+}
